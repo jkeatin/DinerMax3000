4a9ca31a6592dff068fe16d70692200190877c79
James Keating <j.keatin@gmail.com>
2017-12-02 14:53:22 +0000

    order class added

---
+++ DinerMax3000console/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000console
+{
+    public class Order
+    {
+        public List<MenuItem> items = new List<MenuItem>();
+
+        public double Total
+        {
+
+        }
+    }
+}
--- DinerMax3000console/Program.cs
+++ DinerMax3000console/Program.cs
@@ -14,6 +14,27 @@ namespace DinerMax3000console
             summerMenu.Name = "Summer Menu";
             summerMenu.AddMenuItem("Salmon", "Fresh Norwegian Salmon with Sandefjord butter.", 25.50);
             summerMenu.AddMenuItem("Taco","All Norwegians eat taco on Fridays.", 10);
+            summerMenu.HospitalDirections = "Right around the corner of 5th street. Ask for Dr. Jones.";
+
+            DrinkMenu outsideDrinks = new DrinkMenu();
+            outsideDrinks.Disclaimer = "Do not drink and code.";
+            outsideDrinks.AddMenuItem("Virgin Cuba Libre", "A classic.", 10);
+            outsideDrinks.AddMenuItem("Screwdriver", "Makes you hammered.", 15);
+
+            Order hungryGuestOrder = new Order();
+
+            // The two for statements below are both ways of iterating through all items in a list/array
+
+            for (int x = 0; x <= summerMenu.items.Count-1; x++)
+            {
+                MenuItem curentItem = summerMenu.items[x];
+                hungryGuestOrder.items.Add(curentItem);
+            }
+
+            foreach (MenuItem currentItem in outsideDrinks.items)
+            {
+                hungryGuestOrder.items.Add(currentItem);
+            }
         }
     }
 }
