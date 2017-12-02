367cbc3a63b18513ca043b0fbc5c9e4cd5cf4c3a
James Keating <James Keating>
2017-12-02 14:27:20 +0000

    Add project files.

---
+++ DinerMax3000console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu summerMenu = new Menu();
+            summerMenu.Name = "Summer Menu";
+            summerMenu.AddMenuItem("Salmon", "Fresh Norwegian Salmon with Sandefjord butter.", 25.50);
+            summerMenu.AddMenuItem("Taco","All Norwegians eat taco on Fridays.", 10);
+        }
+    }
+}
